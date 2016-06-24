fc1471b0c99bf52dead4bb6d69e57f135c40e33f
LoThanel <remy.ballandras@gmail.com>
2016-06-24 15:22:17 +0200

    CAESAR

--- cryptoweek/src/caesar/Caesar.java
+++ cryptoweek/src/caesar/Caesar.java
@@ -3,16 +3,26 @@ package caesar;
 import java.io.FileReader;
 
 public class Caesar {
-	
+
 	public static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	public static void main(String[] args) {
 		String message = "TESTCAESAR";
+		int decalage = 3;
+		String output = "WHVWFDHVDU";
+		String mode = "decode";
 		String result = "";
-		
-		for(int i = 0; i < message.length(); i ++) {
-			int index = (alphabet.indexOf(message.charAt(i) + 3) % 26);
-			result += alphabet.charAt(index);
+
+		if(mode.equals("encode")) {
+			for(int i = 0; i < message.length(); i ++) {
+				int index = (alphabet.indexOf(message.charAt(i) + decalage) % 26);
+				result += alphabet.charAt(index);
+			}
+		} else if(mode.equals("decode")) {
+			for(int i = 0; i < message.length(); i ++) {
+				int index = (alphabet.indexOf(output.charAt(i) - decalage) % 26);
+				result += alphabet.charAt(index);
+			}
 		}
 		System.out.println(result);
 	}
