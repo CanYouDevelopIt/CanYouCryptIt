930aa0915318ce8bc82d1f20a9f85359aecd4b43
Pascal <pascalnnlacsap@gmail.com>
2016-06-24 15:02:00 +0200

    vigenere

--- cryptoweek/src/vigenere/Vigenere.java
+++ cryptoweek/src/vigenere/Vigenere.java
@@ -2,44 +2,44 @@ package vigenere;
 
 public class Vigenere {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String input = "NGTUBIF";
-        String key = "BC";
-        String mode = "decode";
+		String input = "jadoreecouterlaradiotoutelajournee";
+		input = input.toUpperCase();
+		String key = "MUSIQUE";
+		String mode = "encode";
 
-        String output = "";
+		String output = "";
 
-        //create alphabet table
-        char matrix[][] = new char[58][58];
+		// create alphabet table
+		char matrix[][] = new char[26][26];
 
-        for (int x = 0; x < 58; x++) {
-            for (int y = 0; y < 58; y++) {
-                matrix[x][y] = (char) (((x + y) % 58) + 32);
-            }
-        }
+		for (int x = 0; x < 26; x++) {
+			for (int y = 0; y < 26; y++) {
+				matrix[x][y] = (char) (((x + y) % 26) + 65);
+			}
+		}
 
-        if ("decode".equals(mode)) {
-            for (int i = 0; i < input.length(); i++) {
-                for (int x = 0; x < 58; x++) {
-                    if (matrix[x][key.charAt(i % key.length()) - 32] == input.charAt(i)) {
-                        output += (char) (x + 32);
-                        break;
-                    }
-                }
-            }
-        } else if ("encode".equals(mode)) {
-            for (int i = 0; i < input.length(); i++) {
-                output += matrix[input.charAt(i) - 32][key.charAt(i % key.length()) - 32];
-            }
-        }
-        System.out.println("MODE: " + mode);
-        System.out.println("KEY: " + key);
-        System.out.println("INPUT: " + input);
-        System.out.println();
-        System.out.print("OUTPUT: " + output);
-        System.exit(0);
-    }
-
+		if ("decode".equals(mode)) {
+			for (int i = 0; i < input.length(); i++) {
+				for (int x = 0; x < 26; x++) {
+					if (matrix[x][key.charAt(i % key.length()) - 65] == input.charAt(i)) {
+						output += (char) (x + 65);
+						break;
+					}
+				}
+			}
+		} else if ("encode".equals(mode)) {
+			for (int i = 0; i < input.length(); i++) {
+				output += matrix[input.charAt(i) - 65][key.charAt(i % key.length()) - 65];
+			}
+		}
+		System.out.println("MODE: " + mode);
+		System.out.println("KEY: " + key);
+		System.out.println("INPUT: " + input);
+		System.out.println();
+		System.out.print("OUTPUT: " + output);
+		System.exit(0);
+	}
 
 }
